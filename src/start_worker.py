972f0b2c622eb64b4ad7f519bc66dfcb4777909b
Eesha Kumar <kumar@magazino.eu>
2021-06-21 19:15:18 +0200

    Handle invalid map and reduce ids

--- src/start_worker.py
+++ src/start_worker.py
@@ -12,28 +12,29 @@ from utils import do_map, do_reduce
 def do_task(assigned_task):
     task_type = assigned_task.task_type
     if task_type == TaskType.MAP.value:
-        do_map(assigned_task)
+        do_map(assigned_task.input_filename,
+               assigned_task.M, assigned_task.map_id)
     elif task_type == TaskType.REDUCE.value:
-        do_reduce(assigned_task)
+        do_reduce(assigned_task.reduce_id, assigned_task.N)
     return
 
 
 def main():
     worker_id = str(uuid4())
-    print(worker_id)
+    print("WORKER ID", worker_id)
     channel = grpc.insecure_channel("localhost" + ':50051')
     stub = dist_mr_pb2_grpc.MapReduceDriverStub(channel)
     retry = 3
     task_status = TaskStatus.UNDEFINED.value
     # make some call
-    while(i <= 1):
+    while(True):
         assigned_task = stub.GetTask(
             dist_mr_pb2.WorkerStatus(
                 worker_id=worker_id, worker_status=0, 
                 task_status=task_status), wait_for_ready=True)
         print(assigned_task.task_id, assigned_task.input_filename)
         if not assigned_task.task_id:
-            print("No More Tasks! Retrying....")
+            print("No Task recieved. Retrying....")
             retry -= 1
             if retry == 0:
                 print("No More Tasks, retries exhausted. Quitting...")
--- src/utils.py
+++ src/utils.py
@@ -24,36 +24,38 @@ def read_file_linebyline(f):
     return Path(f).read_text().splitlines()
 
 
-def do_map(input_file, M, map_id):
-    print(f"Working on {input_file} with map id {map_id} and {M} reduceable buckets")
-    buckets = defaultdict([])
-    text = Path(input_file).read_text()
-    all_words = list(re.findall(r"([\w]+['][\w]+)", text))
-    for word in all_words:
-        print(word)
-        buckets[ord(word[0]) % M].append(word)
-    for key, value in buckets.items():
-        text = "\n".join(value)
-        Path(INTERMEDIATE_DIR, f"mr-{map_id}-{key}").write_text(text)
-    print("Map Task completed for {input_file}")
+def do_map(input_file, M, map_id, input_dir=INPUT_DIR):
+    if map_id != -1:
+        print(f"Working on {input_file} with map id {map_id} and {M} reduceable buckets")
+        buckets = defaultdict(list)
+        text = Path(INPUT_DIR, input_file).read_text()
+        all_words = list(re.findall(r"([\w]+['][\w]+)", text))
+        for word in all_words:
+            buckets[ord(word[0]) % M].append(word)
+        for key, value in buckets.items():
+            text = "\n".join(value)
+            Path(INTERMEDIATE_DIR, f"mr-{map_id}-{key}").write_text(text)
+        print(f"Map Task completed for {input_file}")
     return
 
 
 def do_reduce(reduce_id, N):
-    files_to_reduce = []
-    for n in range(N):
-        files_to_reduce.append(os.path.join(INTERMEDIATE_DIR, f"mr-{n}-{reduce_id}"))
-    # all_words = []
-    # start = time.time()
-    # with Pool(5) as p:
-    #     all_words = list(chain(*p.map(read_file_linebyline, files_to_reduce)))
-    all_words = []
-    for f in files_to_reduce:
-        all_words += Path(f).read_text().splitlines()
-    word_counts = Counter(all_words)
-    output_file = os.path.join(OUTPUT_DIR, f"out-{reduce_id}")
-    with open(output_file, 'w+') as f:
-        for word, count in word_counts.items():
-            f.write(f"{word} {count}\n")
-    print("Reduce task completed for {reduce_id}")
+    if reduce_id != -1:
+        print(f"Working on reducing {N} mapped tasks for reduce id {reduce_id}")
+        files_to_reduce = []
+        for n in range(N):
+            files_to_reduce.append(os.path.join(INTERMEDIATE_DIR, f"mr-{n}-{reduce_id}"))
+        # all_words = []
+        # start = time.time()
+        # with Pool(5) as p:
+        #     all_words = list(chain(*p.map(read_file_linebyline, files_to_reduce)))
+        all_words = []
+        for f in files_to_reduce:
+            all_words += Path(f).read_text().splitlines()
+        word_counts = Counter(all_words)
+        output_file = os.path.join(OUTPUT_DIR, f"out-{reduce_id}")
+        with open(output_file, 'w+') as f:
+            for word, count in word_counts.items():
+                f.write(f"{word} {count}\n")
+        print(f"Reduce task completed for {reduce_id}")
     return
