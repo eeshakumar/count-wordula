083f7ab793cecbb27488615dde8f3e735607e422
Eesha Kumar <kumar@magazino.eu>
2021-06-21 10:39:08 +0200

    Driver to manage driver service task delegation

--- src/map_reduce/driver.py
+++ src/map_reduce/driver.py
@@ -1,10 +1,11 @@
-
+from collections import defaultdict
+import uuid
 import multiprocessing
 from map_reduce.task_status import TaskStatus
 
 from utils import collect_map_tasks, collect_reduce_tasks
 from map_reduce.task_type import TaskType
-
+import stub.dist_mr_pb2 as dist_mr_pb2
 
 class Driver(object):
 
@@ -13,42 +14,117 @@ class Driver(object):
         self.M = M
         self.map_task_files = []
         self.reduce_task_files = []
-        self.task_status = multiprocessing.Manager().dict()
+        self.task_status = dict() #multiprocessing.Manager().dict()
 
-    def collect_tasks(self, task_type=TaskType.MAP):
-        if task_type == TaskType.MAP:
+    def collect_tasks(self, task_type=TaskType.MAP.value):
+        if task_type == TaskType.MAP.value:
             self.map_task_files = collect_map_tasks()
             self.build_task_status(task_type)
-        elif task_type == TaskType.REDUCE:
-            reduce_tasks = collect_reduce_tasks()
-            self.build_task_status(reduce_tasks, task_type)
+        elif task_type == TaskType.REDUCE.value:
+            self.reduce_task_files = collect_reduce_tasks()
+            self.build_task_status(task_type)
         else:
             raise AttributeError(f"Task Type {task_type} not found!")
 
     def build_task_status(self, task_type):
+        print("Task type", task_type)
         # while building the task all are status 0, no_started
-        self.task_status[TaskStatus.NOT_STARTED] = {}
-        self.task_status[TaskStatus.NOT_STARTED][task_type] = []
-        if task_type == TaskType.MAP:
+        if task_type == TaskType.MAP.value:
             tasks = self.map_task_files
-        elif task_type == TaskType.REDUCE:
+        elif task_type == TaskType.REDUCE.value:
             tasks = self.reduce_task_files
         else:
             raise AttributeError(f"Task Type {task_type} not found!")
+
+        print("Tasks found", tasks)
+        new_tasks = []
         for task in tasks:
-            self.task_status[TaskStatus.NOT_STARTED][task_type].append(
-                {
+            new_task = {
+                    "task_id": str(uuid.uuid4()),
                     "file_name": task,
                     "worked_id": None,
                 }
-            )
+            new_tasks.append(new_task)
+            print("Append", new_tasks)
 
-    def wait_for_map_tasks(self):
+        if self.task_status.get(TaskStatus.NOT_STARTED.value, None) is None:
+            self.task_status[TaskStatus.NOT_STARTED.value] = {task_type: new_tasks}
+        else:
+            print("Task type", task_type)
+            self.task_status[TaskStatus.NOT_STARTED.value][task_type] = new_tasks
+        print("NA", self.task_status)
+
+        # empty complete and in progress
+        if self.task_status.get(TaskStatus.COMPLETED.value, None) is None:
+            self.task_status[TaskStatus.COMPLETED.value] = {task_type: []}
+        else:
+            print("Task type", task_type)
+            self.task_status[TaskStatus.COMPLETED.value][task_type] = []
+
+        if self.task_status.get(TaskStatus.IN_PROGRESS.value, None) is None:
+            self.task_status[TaskStatus.IN_PROGRESS.value] = {task_type: []}
+        else:
+            self.task_status[TaskStatus.IN_PROGRESS.value][task_type] = []
+
+        print()
+        print()
+        print("FINAL", self.task_status)
+
+    def is_map_tasks_complete(self):
         # check if all tasks are in status 1, completed
-        return sorted(self.map_task_files) == \
-            sorted(self.task_status[TaskStatus.COMPLETED][TaskType.MAP])
+        return len(self.map_task_files) == \
+            len(self.task_status[TaskStatus.COMPLETED.value][TaskType.MAP.value])
  
-    def wait_for_reduce_tasks(self):
+    def is_reduce_tasks_complete(self):
         # check if all tasks are in status 1, completed
-        return sorted(self.map_task_files) == \
-            sorted(self.task_status[TaskStatus.COMPLETED][TaskType.REDUCE])
+        return len(self.map_task_files) == \
+            len(self.task_status[TaskStatus.COMPLETED.value][TaskType.REDUCE.value])
+
+    def get_next_task(self):
+        if not self.is_map_tasks_complete():
+            task_type = TaskType.MAP.value
+        else:
+            task_type = TaskType.REDUCE.value
+
+        try:
+            next_task = self.task_status[TaskStatus.NOT_STARTED.value][task_type].pop(0)
+        except IndexError:
+            next_task = {}
+        return task_type, next_task
+
+    def update_task_status(self, worker_id, filename, old_status, task_type, task_id):
+        if task_id is not None:
+            if old_status == TaskStatus.NOT_STARTED.value:
+                new_status = TaskStatus.IN_PROGRESS.value
+            elif old_status == TaskStatus.IN_PROGRESS.value:
+                new_status = TaskStatus.COMPLETED.value
+        
+            tasks = self.task_status[new_status][task_type]
+            print("Before update", tasks)
+            tasks.append({
+                "task_id": task_id,
+                "file_name": filename,
+                "worker_id": worker_id
+            })
+            print("After update", tasks)
+            self.task_status[new_status]= {task_type: tasks}
+            print('updated task status', self.task_status)
+        
+    def get_prev_task_for_worker(self, worker_id, task_status):
+        # if the status is undefined, the worker is just starting execution, and so attempts a map operation
+        # TODO; It is possible that all map tasks are exhausted.
+        if task_status == TaskStatus.UNDEFINED.value:
+            return TaskType.MAP.value, None
+        prev_task = None
+        for task_type in TaskType:
+            in_progress_tasks = self.task_status[TaskStatus.IN_PROGRESS.value].get(task_type.value, [])
+            print("In prgress tasks", in_progress_tasks)
+            for i, in_progress_task in enumerate(in_progress_tasks):
+                if worker_id == in_progress_task["worker_id"]:
+                    print('Found previous task!!!!')
+                    prev_task = self.task_status[TaskStatus.IN_PROGRESS.value][task_type.value].pop(i)
+                    return task_type.value, prev_task
+    
+    def all_tasks_complete(self):
+        return False
+        # return self.is_map_tasks_complete() #& self.is_reduce_tasks_complete()
--- src/map_reduce/driver_service.py
+++ src/map_reduce/driver_service.py
@@ -1,18 +1,45 @@
+from map_reduce.task_type import TaskType
+from map_reduce.task_status import TaskStatus
 
 import stub.dist_mr_pb2
 import stub.dist_mr_pb2_grpc
+
+
+INPUT_DIR = "./input"
+OUTPUT_DIR = "./out"
+INTERMEDIATE_DIR = "./intermediate"
+
+
+def build_task(task_type, next_task, N, M):
+    # build task into Task obj
+    return stub.dist_mr_pb2.Task(task_id=next_task.get("task_id", None),
+            task_type=task_type, input_dir=INPUT_DIR,
+            input_filename=next_task.get("file_name", ""),
+            output_dir=OUTPUT_DIR, N=N, M=M)
 
 
 class DriverService(stub.dist_mr_pb2_grpc.MapReduceDriverServicer):
 
     def __init__(self, driver, *args, **kwargs):
         self.driver = driver
+        self.driver.collect_tasks()
+        self.driver.collect_tasks(TaskType.REDUCE.value)
         # super().__init__(args, kwargs)
 
     def GetTask(self, request, context):
-        print("Recieved REQ", request.worker_id, request.worker_status)
-        return stub.dist_mr_pb2.Task(task_id=1,
-            task_type=0, input_dir='./input',
-            input_filename='test_filename',
-            output_dir='', N=3, M=4)
-
+        print()
+        print("Recieved REQ", request.worker_id, request.worker_status, request.task_status)
+        task_type, prev_task = self.driver.get_prev_task_for_worker(request.worker_id, request.task_status)
+        if prev_task is not None:
+            print("PREV TASK", prev_task)
+            self.driver.update_task_status(
+                request.worker_id, prev_task["file_name"], TaskStatus.IN_PROGRESS.value, 
+                task_type, prev_task["task_id"])
+        task_type, next_task = self.driver.get_next_task()
+        print("Recieved next task", next_task)
+        task = build_task(task_type, next_task, self.driver.N, self.driver.M)
+        self.driver.update_task_status(request.worker_id, task.input_filename, 
+                                       TaskStatus.NOT_STARTED.value, task.task_type,
+                                       next_task.get("task_id", None))
+        print(request.worker_id, next_task["file_name"])
+        return task
