e05b6ff19cb92cde5fea090afa1044f9cb985aad
Eesha Kumar <kumar@magazino.eu>
2021-06-21 12:28:00 +0200

    data moved to src

--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,8 @@ import os
 
 
 def collect_map_tasks():
-    return os.listdir("/data/input")
+    return os.listdir(os.path.join(os.getcwd(), "data/inputs"))
 
 
 def collect_reduce_tasks():
-    return os.listdir("/data/intermediate")
+    return os.listdir(os.path.join(os.getcwd(), "data/intermediate"))
